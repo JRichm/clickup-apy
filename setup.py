f74006ced0e3fd218419c2b4a79c7d95e2fcb1d0
JRichm <jblackenmusic@gmail.com>
2025-09-21 15:46:46 -0600

    fix typo lmao

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packeges
+from setuptools import setup, find_packages
 
 setup(
     name="clickup-apy",
     version="0.1.0",
-    packages=find_packeges(),
+    packages=find_packages(),
     install_requires=[
         "requests",
         "python-dotenv"
